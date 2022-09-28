de4bc281446129467ce8000e8b172e17e498f972
curtis <huangguocai9533@xwtec.cn>
2022-09-29 02:18:02 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+## [1.0.0] 
+
+* initRelease
+
+## [1.0.1] 
+
+* add example
+
+## [1.0.2] 
+
+* support flutter v2.0
+
+## [1.0.3] 
+
+* change pubspec.yaml
+
+## [1.0.4] 
+
+* support Null-safety
+
+## [1.0.5] 
+
+* remove print from lib/src/widget/date_picker_widget.dart
+
+## [1.0.6] 
+
+* change readme.md
---
+++ example/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+import './date_picker_bottom_sheet.dart';
+import './date_picker_in_page.dart';
+import './datetime_picker_bottom_sheet.dart';
+import './datetime_picker_in_page.dart';
+import './time_picker_bottom_sheet.dart';
+import './time_picker_in_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Date Picker Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = TextStyle(color: Colors.white, fontSize: 16.0);
+    return Scaffold(
+      appBar: AppBar(title: Text('Date Picker Demo')),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text("DatePicker Bottom Sheet", style: textStyle),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return DatePickerBottomSheet();
+                }));
+              },
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text("DatePicker In Page", style: textStyle),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return DatePickerInPage();
+                }));
+              },
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text("TimePicker Bottom Sheet", style: textStyle),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return TimePickerBottomSheet();
+                }));
+              },
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text("TimePicker In Page", style: textStyle),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return TimePickerInPage();
+                }));
+              },
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text("DateTimePicker Bottom Sheet", style: textStyle),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return DateTimePickerBottomSheet();
+                }));
+              },
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              child: Text("DateTimePicker In Page", style: textStyle),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return DateTimePickerInPage();
+                }));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
